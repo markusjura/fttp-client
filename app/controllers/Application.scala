d80a8ecfa355fb9ccc91c143910eefe8d272d8dc
Markus Jura <markus.jura@gmail.com>
2014-04-07 14:19:10 +0200

    Login form without auth service

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,7 +15,38 @@ import play.api.libs.json._
 
 object Application extends Controller {
 
-  def index = Action { implicit request =>
-    Ok(views.html.index())
+  val usernameSessionKey = "username"
+  def requestUsername(request: RequestHeader) = request.session.get(usernameSessionKey)
+  def onUnauthorized(request: RequestHeader) = Redirect(routes.Application.login)
+
+  def index = Authenticated(requestUsername, onUnauthorized) { username =>
+    Action { implicit request =>
+      Ok(views.html.index())
+    }
+  }
+
+  val loginForm: Form[LoginCredentials] = Form {
+    mapping(
+      "username" -> nonEmptyText(minLength = 4),
+      "password" -> nonEmptyText(minLength = 4)
+    )(LoginCredentials.apply)(LoginCredentials.unapply)
+  }
+
+  def login = Action { implicit request =>
+    Ok(views.html.login(loginForm))
+  }
+
+  def submit = Action { implicit request =>
+    def onError(formWithErrors: Form[LoginCredentials]) =
+      BadRequest(views.html.login(formWithErrors))
+
+    def onSuccess(credentials: LoginCredentials) =
+      Redirect(routes.Application.index).withSession(usernameSessionKey -> credentials.username)
+
+    loginForm.bindFromRequest().fold(onError, onSuccess)
+  }
+
+  def logout = Action { implicit request =>
+    Redirect(routes.Application.login).withNewSession
   }
 }
--- app/models/LoginCredentials.scala
+++ app/models/LoginCredentials.scala
@@ -2,3 +2,4 @@ package models
 
 import play.api.libs.json.Json
 
+case class LoginCredentials(username: String, password: String)
